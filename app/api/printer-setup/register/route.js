e22fe18e33f36cb429d99a0e5cb00d6f72692fd8
Satyam kumar <satyamkumar2716@gmail.com>
2026-08-08 01:32:50 +0530

    updated

--- app/api/printer-setup/register/route.js
+++ app/api/printer-setup/register/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import  connectDB  from '@/lib/mongodb';
+import connectDB from '@/lib/db';
 import PrinterRegistration from '@/models/PrinterRegistration';
 import { sendEmail } from '@/lib/emailService';
 
